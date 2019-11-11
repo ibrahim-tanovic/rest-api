25a97def80a6cd2da64f3bb6d5f8e9449d6c8f58
Ibrahim Tanović <ibrahim.tanovic@gmail.com>
2019-11-11 17:53:14 +0100

    added sample data db dump

---
+++ sql/products.sql
@@ -0,0 +1,4 @@
+INSERT INTO products_db.product (id, name, price, rating, updated_at, color) VALUES (1, 'banana smoothie', 690, 4.2, '2019-11-11 14:36:51', 'yellow');
+INSERT INTO products_db.product (id, name, price, rating, updated_at, color) VALUES (2, 'kale smoothie', 585, 4.8, '2019-11-11 15:02:35', 'green');
+INSERT INTO products_db.product (id, name, price, rating, updated_at, color) VALUES (3, 'apple smoothie', 380, 4.5, '2019-11-11 15:02:35', 'yellow');
+INSERT INTO products_db.product (id, name, price, rating, updated_at, color) VALUES (4, 'orange smoothie', 250, 4.2, '2019-11-11 15:02:35', 'orange');
